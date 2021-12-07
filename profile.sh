73b6ecceec2e733d3a099a9952f3d4c74cc59170
John Franklin Crenshaw <jfcrenshaw@John-Franklins-MacBook-Pro.local>
2021-12-07 00:02:04 -0800

    Added basic zsh configs.

---
+++ profile.sh
@@ -0,0 +1,22 @@
+# setup homebrew
+if [ -e /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# path to local programs
+LOCAL_BIN="$HOME/.local/bin"
+if [ -d "$LOCAL_BIN" ]; then
+    export PATH="$LOCAL_BIN:$PATH"
+fi
+
+# cuda path
+CUDA_PATH="/usr/local/cuda"
+if [ -d "$CUDA_PATH" ]; then
+    export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
+    export PATH="$CUDA_PATH:$PATH"
+fi
+
+# if running a different shell, switch to zsh
+if [ -z "$ZSH_VERSION" ]; then
+    exec zsh
+fi
