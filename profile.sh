d0568882964d220350f12fc79ba48710d48eba38
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2026-05-02 13:19:13 -0700

    Fixes for NERSC

--- profile.sh
+++ profile.sh
@@ -5,12 +5,12 @@ fi
 
 # path to local binaries
 LOCAL_BIN="$HOME/.local/bin"
-if [ -d "$LOCAL_BIN" ]; then
+if [ -d "$LOCAL_BIN" ] && [[ ":$PATH:" != *":$LOCAL_BIN:"* ]]; then
     export PATH="$LOCAL_BIN:$PATH"
 fi
 
 # cuda path
-unset LD_LIBRARY_PATH
+# unset LD_LIBRARY_PATH
 #CUDA_PATH="/usr/local/cuda"
 #if [ -d "$CUDA_PATH" ]; then
 #    export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
@@ -18,9 +18,9 @@ unset LD_LIBRARY_PATH
 #fi
 
 # if running a different shell, switch to zsh (if it exists)
-if [ -z "$ZSH_VERSION" ]; then # if not running zsh
-    if type zsh > /dev/null; then # check if zsh exists
-        export SHELL=`which zsh` # set the shell to the zsh in path
-        exec $SHELL # execute zsh
-    fi
-fi
+#if [ -z "$ZSH_VERSION" ]; then # if not running zsh
+#    if type zsh > /dev/null; then # check if zsh exists
+#        export SHELL=`which zsh` # set the shell to the zsh in path
+#        exec $SHELL # execute zsh
+#    fi
+#fi
